5a9e4febb2f4d06503489b9b29cb52bbc3c322f6
Nelca <m.n.93710167@gmail.com>
2017-08-11 08:47:06 +0900

    modify read saved moedl functions

---
+++ clusteringMusicTest/read_saved_models.py
@@ -0,0 +1,22 @@
+from sklearn.externals import joblib
+from keras.models import load_model
+
+
+def loadMfcc3dStanderdScaler():
+    saved_ss = joblib.load('./savedStanderdScaler/mfcc_3d_ss.pkl')
+    return saved_ss
+
+
+def loadKerasModel(file_name):
+    loaded_model = load_model('./savedModels/' + file_name + '.h5')
+    return loaded_model
+
+
+def loadCepsDenseModel():
+    loaded_model = loadKerasModel('ceps_dense_model')
+    return loaded_model
+
+def loadMfcc3dCnnModel():
+    loaded_model = loadKerasModel('ceps_cnn3d_model')
+    return loaded_model
+
